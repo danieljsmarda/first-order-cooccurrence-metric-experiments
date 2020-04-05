ed9fe8b8621129d46eff941b646f3cf74590442b
Daniel Smarda <danieljsmarda@gmail.com>
2020-04-05 21:54:09 +0200

    Add 1st-order pvalue

--- src/models/effect_size.py
+++ src/models/effect_size.py
@@ -4,6 +4,7 @@ from scipy.spatial.distance import cosine as cosine_distance
 from scipy.special import comb as num_combinations
 from itertools import combinations
 from tqdm import tqdm
+from statistics import mean
 import numpy as np
 import scipy as sp
 
@@ -34,7 +35,7 @@ def calculate_effect_size(X_mtx, Y_mtx, A_mtx, B_mtx):
     all_associations = np.apply_along_axis(lambda w_vec: calculate_association_metric_for_target_word(w_vec, A_mtx, B_mtx), 1, X_union_Y)
     return (np.mean(x_associations) - np.mean(y_associations))/np.std(all_associations, ddof=1)
 
-def produce_effect_size(wv_obj, X_terms, Y_terms, A_terms, B_terms):
+def produce_2ndorder_effect_size(wv_obj, X_terms, Y_terms, A_terms, B_terms):
     '''Highest-level function, from word-vector object to output effect size.'''
     [X_mtx, Y_mtx, A_mtx, B_mtx] = get_matrices_from_term_lists(wv_obj, X_terms, Y_terms, A_terms, B_terms)
     return calculate_effect_size(X_mtx, Y_mtx, A_mtx, B_mtx)
@@ -56,7 +57,7 @@ def get_complements(x_union_y):
         complement = frozenset(x_union_y.difference(seq))
         yield (seq, complement)
 
-def produce_p_value(wv_obj, X_terms, Y_terms, A_terms, B_terms):
+def produce_2ndorder_p_value(wv_obj, X_terms, Y_terms, A_terms, B_terms):
     '''Generates the p-value for a set of terms with the word-vector object.
     High-level function; this function should be directly imported into 
     notebooks for experimentation.'''
@@ -66,5 +67,69 @@ def produce_p_value(wv_obj, X_terms, Y_terms, A_terms, B_terms):
     dist = np.array([])
     for (X_i_terms, Y_i_terms) in tqdm(get_complements(x_union_y), total=num_combinations(total_terms, total_terms/2)):
         test_statistic = produce_test_statistic(wv_obj, X_i_terms, Y_i_terms, A_terms, B_terms)
+        dist = np.append(dist, test_statistic)
+    return 1 - sp.stats.norm.cdf(comparison_statistic, loc=np.mean(dist), scale=np.std(dist, ddof=1))
+
+############## 1st-order #####################
+def get_expSG_vecs(words, we_model, E_ctx_vec, E_wrd_vec):
+    
+    expSG_vecs = {}
+    for word in words:
+        _idx = we_model.wv.vocab[word].index
+        _vec = we_model.wv.vectors[_idx]
+        
+        # explicit SkipGram
+        expSG_vecs[word] = sp.special.expit(np.dot(we_model.trainables.syn1neg, _vec))
+        expSG_vecs[word] /= np.sqrt(E_ctx_vec * E_wrd_vec[_idx])
+    
+    return expSG_vecs
+
+def get_expSG_1storder_relation(word_from, words_to, we_model, E_ctx_vec, E_wrd_vec):
+    expSG_vec = get_expSG_vecs([word_from], we_model, E_ctx_vec, E_wrd_vec)[word_from]
+    
+    relations={}
+    for word_to in words_to:
+        if word_to in we_model.wv.vocab:
+            _idx = we_model.wv.vocab[word_to].index
+            relations[word_to] = expSG_vec[_idx]
+    
+    return relations
+
+def get_1storder_association_metric(word, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec):
+    A_relations = get_expSG_1storder_relation(word, A_terms, we_model, E_ctx_vec, E_wrd_vec)
+    B_relations = get_expSG_1storder_relation(word, B_terms, we_model, E_ctx_vec, E_wrd_vec)
+    return mean(A_relations.values()) - mean(B_relations.values())
+
+def produce_1storder_effect_size_unnormalized(X_terms, Y_terms, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec):
+    x_associations = np.array([])
+    y_associations = np.array([])
+    for (x,y) in zip(X_terms, Y_terms):
+        x_association = get_1storder_association_metric(x, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec)
+        y_association = get_1storder_association_metric(y, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec)
+        x_associations = np.append(x_associations, x_association)
+        y_associations = np.append(y_associations, y_association)
+    all_associations = np.append(x_associations, y_associations)
+    return (np.mean(x_associations) - np.mean(y_associations))/np.std(all_associations, ddof=1)
+
+def produce_1storder_test_statistic(we_model, X_terms, Y_terms, A_terms, B_terms, E_ctx_vec, E_wrd_vec):
+    x_associations = np.array([])
+    y_associations = np.array([])
+    for (x,y) in zip(X_terms, Y_terms):
+        x_association = get_1storder_association_metric(x, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec)
+        y_association = get_1storder_association_metric(y, A_terms, B_terms, we_model, E_ctx_vec, E_wrd_vec)
+        x_associations = np.append(x_associations, x_association)
+        y_associations = np.append(y_associations, y_association)
+    return np.sum(x_associations) - np.sum(y_associations)
+
+def produce_1storder_p_value(we_model, X_terms, Y_terms, A_terms, B_terms, E_ctx_vec, E_wrd_vec):
+    '''Generates the p-value for a set of terms with the word-vector object.
+    High-level function; this function should be directly imported into 
+    notebooks for experimentation.'''
+    x_union_y = set(X_terms).union(set(Y_terms))
+    total_terms = len(x_union_y)
+    comparison_statistic = produce_1storder_test_statistic(we_model, X_terms, Y_terms, A_terms, B_terms, E_ctx_vec, E_wrd_vec)
+    dist = np.array([])
+    for (X_i_terms, Y_i_terms) in tqdm(get_complements(x_union_y), total=num_combinations(total_terms, total_terms/2)):
+        test_statistic = produce_1storder_test_statistic(we_model, X_i_terms, Y_i_terms, A_terms, B_terms, E_ctx_vec, E_wrd_vec)
         dist = np.append(dist, test_statistic)
     return 1 - sp.stats.norm.cdf(comparison_statistic, loc=np.mean(dist), scale=np.std(dist, ddof=1))
