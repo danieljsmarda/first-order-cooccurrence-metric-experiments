f9d335a8de5a4535738fd4206d66fbef97a85803
Daniel Smarda <danieljsmarda@gmail.com>
2020-08-26 17:27:07 +0200

    Calculate and visualize 90% CI

--- src/models/utils.py
+++ src/models/utils.py
@@ -13,7 +13,8 @@ def open_pickle(FILEPATH):
     f.close()
     return obj
 
-def save_arrays(FILEPATH, exp_num, order, X_metrics, Y_metrics, threshold):
+def save_arrays(FILEPATH, exp_num, order, X_metrics, Y_metrics, threshold,
+    pct_5, pct_95):
     results_dict = open_pickle(FILEPATH)
     results_dict[exp_num] = results_dict.get(exp_num, defaultdict(dict))
     order_dict = results_dict[exp_num].get(order, {})
@@ -22,8 +23,11 @@ def save_arrays(FILEPATH, exp_num, order, X_metrics, Y_metrics, threshold):
     order_dict['X_mean'] = np.mean(X_metrics)
     order_dict['Y_mean'] = np.mean(Y_metrics)
     order_dict['threshold'] = threshold
+    order_dict['pct_5'] = pct_5
+    order_dict['pct_95'] = pct_95
     results_dict[exp_num][order] = order_dict
     save_pickle(results_dict, FILEPATH)
+    print(f'RESULTS DICT FOR EXP {exp_num}', results_dict[exp_num])
     print(f"Results array successfully saved to file {FILEPATH} under\
  keys [{exp_num}][{order}]")
 
